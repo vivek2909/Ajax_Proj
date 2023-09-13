8535d81055041e9011ceee2699468e18fe8abd0f
vivek2909 <vivekrokade@nimapinfotech.com>
2023-09-13 12:39:38 +0530

    Create New Event and added Coding.

---
+++ WebForm2.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Ajax_Proj
+{
+    public partial class WebForm2 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            int num1 = int.Parse(TextBox1.Text);
+            int num2 = int.Parse(TextBox2.Text);
+            int result = num1 + num2;
+            Label3.Text = result.ToString();
+            Label3.Visible = true;
+        }
+    }
+}
